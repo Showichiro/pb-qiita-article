a5cd3965d12e36be04e2ebcb00e245a697a1b5d2
uchihori <show2305@gmail.com>
2024-03-26 03:48:13 +0900

    tmp

--- data/articles/insert-4.sql
+++ data/articles/insert-4.sql
@@ -1,4 +1,4 @@
-INSERT INTO articles(id,title,user_id,user_name,created_at,likes_count,stocks_count) VALUES('cf817514cbd55a027d70','Gitで不要になったリモートブランチを削除','ryotaro76','riga','2016-09-30T15:41:40+09:00',2,0); 
-INSERT INTO articles(id,title,user_id,user_name,created_at,likes_count,stocks_count) VALUES('d457c4a79715115e1a42','Spring-boot の覚書','ryotaro76','riga','2016-09-15T08:19:38+09:00',1,1); 
-INSERT INTO articles(id,title,user_id,user_name,created_at,likes_count,stocks_count) VALUES('139ed17f65f2581f8de9','よく使うワンライナー','ryotaro76','riga','2016-09-13T15:01:26+09:00',3,3); 
-INSERT INTO articles(id,title,user_id,user_name,created_at,likes_count,stocks_count) VALUES('7887d619856d0ead13e1','チェックボックスをすべてチェックする','ryotaro76','riga','2016-09-01T16:16:22+09:00',1,3); 
+INSERT OR REPLACE INTO articles(id,title,user_id,user_name,created_at,likes_count,stocks_count) VALUES('cf817514cbd55a027d70','Gitで不要になったリモートブランチを削除','ryotaro76','riga','2016-09-30T15:41:40+09:00',2,0); 
+INSERT OR REPLACE INTO articles(id,title,user_id,user_name,created_at,likes_count,stocks_count) VALUES('d457c4a79715115e1a42','Spring-boot の覚書','ryotaro76','riga','2016-09-15T08:19:38+09:00',1,1); 
+INSERT OR REPLACE INTO articles(id,title,user_id,user_name,created_at,likes_count,stocks_count) VALUES('139ed17f65f2581f8de9','よく使うワンライナー','ryotaro76','riga','2016-09-13T15:01:26+09:00',3,3); 
+INSERT OR REPLACE INTO articles(id,title,user_id,user_name,created_at,likes_count,stocks_count) VALUES('7887d619856d0ead13e1','チェックボックスをすべてチェックする','ryotaro76','riga','2016-09-01T16:16:22+09:00',1,3); 
