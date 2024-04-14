7d18c1d1e2efd931bfa3e28762975b468882dd70
Showichiro <65388586+Showichiro@users.noreply.github.com>
2024-04-14 22:08:26 +0000

    Update SQL

--- data/articles/insert-3.sql
+++ data/articles/insert-3.sql
@@ -1,3 +1,4 @@
+INSERT OR REPLACE INTO articles(id,title,user_id,user_name,created_at,likes_count,stocks_count) VALUES('c7948dd9470abfaa4a79','UiPath Orchestratorに接続されたRobot/Studioのライセンシングについて備忘。','masatomix','Masatomi KINO','2019-06-06T15:22:32+09:00',13,8); 
 INSERT OR REPLACE INTO articles(id,title,user_id,user_name,created_at,likes_count,stocks_count) VALUES('0678e56e56af27efa110','誤差逆伝播ででてくるアフィン変換の微分演算の導出と、Pythonで実際に計算してみた','masatomix','Masatomi KINO','2019-06-05T11:05:27+09:00',29,32); 
 INSERT OR REPLACE INTO articles(id,title,user_id,user_name,created_at,likes_count,stocks_count) VALUES('146ab67dd91263898f5b','Qiitaの下書きで、数式プレビューが無効になった？？','masatomix','Masatomi KINO','2019-06-01T14:53:23+09:00',1,0); 
 INSERT OR REPLACE INTO articles(id,title,user_id,user_name,created_at,likes_count,stocks_count) VALUES('6c8405fada7a6fead7cc','Spring Security で「Hello World！」','ryotaro76','riga','2019-05-20T17:40:34+09:00',12,10); 
@@ -97,4 +98,3 @@ INSERT OR REPLACE INTO articles(id,title,user_id,user_name,created_at,likes_coun
 INSERT OR REPLACE INTO articles(id,title,user_id,user_name,created_at,likes_count,stocks_count) VALUES('1a02c767a949959c0b3b','WindowsのリモートデスクトップでCtrl＋Alt＋Delを送信する','ryotaro76','riga','2017-01-31T10:04:11+09:00',0,0); 
 INSERT OR REPLACE INTO articles(id,title,user_id,user_name,created_at,likes_count,stocks_count) VALUES('c1f8674213500f54df0f','JavaでSSLでRESTを投げるときに、プロキシを通す方法','masatomix','Masatomi KINO','2017-01-03T12:51:04+09:00',4,6); 
 INSERT OR REPLACE INTO articles(id,title,user_id,user_name,created_at,likes_count,stocks_count) VALUES('4bdb5f25de632c97f37f','GitHubのマージとリベースについて','masatomix','Masatomi KINO','2016-12-29T07:35:47+09:00',11,10); 
-INSERT OR REPLACE INTO articles(id,title,user_id,user_name,created_at,likes_count,stocks_count) VALUES('87d0d5f72668d9c76089','Amazon Dash Button でSlackにデータを送信する','masatomix','Masatomi KINO','2016-12-11T14:02:20+09:00',5,10); 
